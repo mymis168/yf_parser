a59c550888fe188be582be7bd3283f73131b62cd
Michael <mymis168@gmail.com>
2026-08-19 14:23:45 +0800

    matplot均線圖版本

--- annual_sample.py
+++ annual_sample.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 from pprint import pp, pprint
+import matplotlib.pyplot as plt
+
 
 # pandas DataFrame 下載台積電股票歷史股價數據 的功能
 
@@ -7,11 +9,23 @@ data = yf.download(["2330.TW"], start="2026-03-01", end="2026-05-01", interval="
 # 顯示 統計數據中的 移動平均線(股票常用 5日、10日、20日、60日、120日、240日) 以及 最高價、最低價、收盤價
 pprint(data['Close']['2330.TW'])  # 顯示收盤
 # 五日均值
+data['MA5'] = data['Close']['2330.TW'].rolling(window=5).mean()
 pprint(f"5-day Moving Average: {data['Close']['2330.TW'].rolling(window=5).mean().iloc[-1]:.2%}")
 # 十日均值
+data['MA10'] = data['Close']['2330.TW'].rolling(window=10).mean()
 pprint(f"10-day Moving Average: {data['Close']['2330.TW'].rolling(window=10).mean().iloc[-1]:.2%}")
 # 二十日均值
+data['MA20'] = data['Close']['2330.TW'].rolling(window=20).mean()
 pprint(f"20-day Moving Average: {data['Close']['2330.TW'].rolling(window=20).mean().iloc[-1]}")
+
+#繪製 5日、10日、20日均線圖表
+data[['Close','MA5','MA10','MA20']].plot(figsize=(12, 6))
+plt.title('台積電 5日、10日、20日均線圖表')
+plt.ylabel('價格') # 設定 y軸資料名稱
+plt.show()   # 將圖表顯示出來
+
+
+
 
 #每日報酬率
 
